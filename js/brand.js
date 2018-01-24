0b6acd2038b06e78fa7f08a44a865400395ffe7f
Frances Botsford <frances@switchingprotocols.com>
2018-01-24 14:51:24 -0500

    switch from local storage to cookie

--- js/brand.js
+++ js/brand.js
@@ -1,26 +1,43 @@
 $(function(){
 
-	// What variable are we using to store toggle status?
-	var toggle = 'brand_closed';
+// cookie set up from PPK https://www.quirksmode.org/js/cookies.html
+  function createCookie(name,value,days) {
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+  }
 
-	// This checks if the user has already closed the branding header.
-	// If localStorage
-	if (Modernizr.localstorage) {
-		// Check for the localStorage alert ID item
-		if (localStorage.getItem(toggle) === 'true') {
-			$('.brand-splash').removeClass('big').addClass('compact');
-		}
-	}
+  function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
 
-	// On click
+  function eraseCookie(name) {
+    createCookie(name,"",-1);
+  }
+
+  var splashStatus = readCookie('brand_closed');
+  if (splashStatus == 'true') {
+  	$('.brand-splash').removeClass('big').addClass('compact');
+  } 
+
 	$('.btn-minimize').click(function(){
 		$('.brand-splash').removeClass('big').addClass('compact');
-		if (Modernizr.localstorage) {
-			// Set the localStorage item, using the post ID
-			localStorage.setItem(toggle, 'true');
-		}
-		// Prevent the default click behavior.
+		createCookie('brand_closed','true',99999);
 		return false;
 	});
+
+
+
 
 });
